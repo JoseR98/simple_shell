1309e15dad82efbfd363000ac553ed5536552371
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-14 20:02:17 +0000

    Modify prompt prototype

--- main.c
+++ main.c
@@ -9,11 +9,10 @@ int main(int argc, char *argv[], char *env[])
 	int num;
 	(void)argc;
 	/* general commands */
-	char *var[3] = {"exit", "env", "clear"};
 	/*            */
 	if (isatty(STDIN_FILENO))
 		write(STDOUT_FILENO, USER, 2);
-	num = prompt(argv, env, var);
+	num = prompt(argv, env);
 	if (num == -1)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
