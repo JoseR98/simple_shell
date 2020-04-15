dc5d417c8bf73aeff3f077312d4c6bcdf679a3ba
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-15 18:53:13 +0000

    Add error for illegal arguments for exit command

--- print_error.c
+++ print_error.c
@@ -42,18 +42,36 @@ char *own_itoa(int num_command)
 void c_not_found(char **commands, char *buffer, char **argv, int num_command)
 {
 	char *num_com;
-	(void)argv;
 
-	write(STDERR_FILENO, argv[0], _strlen(argv[0]));
-	write(STDERR_FILENO, ": ", 2);
-	num_com = own_itoa(num_command);
-	write(STDERR_FILENO, num_com, _strlen(num_com));
-	write(STDERR_FILENO, ": ", 2);
-	write(STDERR_FILENO, commands[0], _strlen(commands[0]));
-	write(STDERR_FILENO, ": not found\n", 12);
-
+	if ((_strcmp(commands[0], "exit") == 0) && commands[1])
+	{
+		write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+		write(STDERR_FILENO, ": ", 2);
+		num_com = own_itoa(num_command);
+		write(STDERR_FILENO, num_com, _strlen(num_com));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, commands[0], _strlen(commands[0]));
+		write(STDERR_FILENO, ": Illegal number: ", 18);
+		write(STDERR_FILENO, commands[1], _strlen(commands[1]));
+		write(STDERR_FILENO, "\n", 1);
+		free(num_com);
+		free(buffer);
+		free_commands(commands);
+		exit(2);
+	}
+	else
+	{
+		write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+		write(STDERR_FILENO, ": ", 2);
+		num_com = own_itoa(num_command);
+		write(STDERR_FILENO, num_com, _strlen(num_com));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, commands[0], _strlen(commands[0]));
+		write(STDERR_FILENO, ": not found\n", 12);
+	}
 	free(num_com);
 	free(buffer);
 	free_commands(commands);
 	exit(127);
 }
+
